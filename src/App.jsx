2b0638cdc54aa1ca6aed83b30612c590bdf26c49
Diego Efrén Vázquez Gómez <defren3211@gmail.com>
2025-05-31 19:26:08 -0600

    Toaster Añadido a la Aplicación
    chores: Agregando el toaster de la biblioteca react-hot-toast para notificaciones.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 import HomePage from '@/HomePage'
 import NotFound404 from '@/NotFound404'
@@ -12,60 +13,66 @@ function App() {
   const navigate = useNavigate()
 
   return (
-    <div className='container mx-auto'>
-      <div className='navbar bg-primary text-primary-content shadow-sm w-full rounded-2xl'>
-        <div className='flex flex-1 items-center'>
-          <Link
-            className='btn btn-ghost text-xl'
-            to='/'
-          >
-            GDM
-          </Link>
+    <>
+      <div className='container mx-auto'>
+        <div className='navbar bg-primary text-primary-content shadow-sm w-full rounded-2xl'>
+          <div className='flex flex-1 items-center'>
+            <Link
+              className='btn btn-ghost text-xl'
+              to='/'
+            >
+              GDM
+            </Link>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                navigate('/')
+                setTimeout(() => navigate('/form-alumno'), 0)
+              }}
+              className='btn btn-warning mx-1'
+            >
+              Formulario de Registro
+            </button>
+          </div>
         </div>
-        <div>
-          <button
-            onClick={() => {
-              navigate('/')
-              setTimeout(() => navigate('/form-alumno'), 0)
-            }}
-            className='btn btn-warning mx-1'
-          >
-            Formulario de Registro
-          </button>
-        </div>
-      </div>
 
-      <Routes>
-        <Route
-          path='/'
-          element={<HomePage />}
-        />
-        <Route
-          path='form-alumno'
-          element={<FormAlumno />}
-        />
-        <Route
-          path='form-tutores'
-          element={<FormTutores />}
-        />
-        <Route
-          path='form-hermanos'
-          element={<FormHermanos />}
-        />
-        <Route
-          path='form-contacto'
-          element={<FormContacto />}
-        />
-        <Route
-          path='form-pago'
-          element={<FormPago />}
-        />
-        <Route
-          path='*'
-          element={<NotFound404 />}
-        />
-      </Routes>
-    </div>
+        <Routes>
+          <Route
+            path='/'
+            element={<HomePage />}
+          />
+          <Route
+            path='form-alumno'
+            element={<FormAlumno />}
+          />
+          <Route
+            path='form-tutores'
+            element={<FormTutores />}
+          />
+          <Route
+            path='form-hermanos'
+            element={<FormHermanos />}
+          />
+          <Route
+            path='form-contacto'
+            element={<FormContacto />}
+          />
+          <Route
+            path='form-pago'
+            element={<FormPago />}
+          />
+          <Route
+            path='*'
+            element={<NotFound404 />}
+          />
+        </Routes>
+      </div>
+      <Toaster
+        position='top-center'
+        reverseOrder={true}
+      />
+    </>
   )
 }
 
