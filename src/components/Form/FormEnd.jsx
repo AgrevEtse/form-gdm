ce99cff4ab67e6dcd24a5ed450c70f67b1bc5001
Diego Efrén Vázquez Gómez <defren3211@gmail.com>
2025-06-12 19:45:37 -0600

    Cambiando leyenda del FormEnd

--- src/components/Form/FormEnd.jsx
+++ src/components/Form/FormEnd.jsx
@@ -16,8 +16,11 @@ const FormEnd = forwardRef(() => {
     <div className='flex items-center justify-center p-4'>
       <div className='bg-primary text-primary-content shadow-2xl rounded-2xl p-8 max-w-md w-full text-center'>
         <h2 className='text-5xl font-extrabold mb-4'>Registro Terminado</h2>
-        <p className='mb-6'>
+        <p className='mb-3'>
           Concluiste satifactoriamente el registro del alumno.
+        </p>
+        <p className='mb-6 font-bold'>
+          La inscripción termina con el pago de la misma.
         </p>
         <div className='flex flex-row justify-center gap-4'>
           <Link to='https://gomezdemendiola.edu.mx/'>
